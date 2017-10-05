e6dbbbac0f433dc01983e36622b2caaa54a384e7
Randy Fay <randy@randyfay.com>
2017-10-05 10:26:07 -0600

    Working with MyTextFormatter

--- cmd/logrustry/main.go
+++ cmd/logrustry/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"os"
+
 	log "github.com/sirupsen/logrus"
 )
 
+// Overall strategy:
+/**
+* Add formatters for user interaction, normal logging, and full json interaction
+* Use log for all output aimed at the user
+* Remove all imports of the stdlib log
+* Remove all usages of fmt.Print* and the like
+
+Consider one logger for user output (what we *expect* them to see) and another for error/warn output.
+One could be called userout? The other errorout?
+*/
+
+var logger = log.New()
+
 func init() {
+
 	// Log as JSON instead of the default ASCII formatter.
 	//log.SetFormatter(&log.JSONFormatter{})
 
@@ -14,14 +29,34 @@ func init() {
 	log.SetOutput(os.Stdout)
 
 	// Only log the warning severity or above.
-	log.SetLevel(log.WarnLevel)
+	log.SetLevel(log.DebugLevel)
+
+	//x := logger.(*log.MyTextFormatter).DisableTimestamp
+	//x := log.MyTextFormatter(logger)
+	//x := logger.(*log.MyTextFormatter)
 }
 
 func main() {
+	log.Warn("Try this")
+	log.Print("This is just a print")
+	customFormatter := new(MyTextFormatter)
+	customFormatter.DisableTimestamp = true
+	customFormatter.isTerminal = false
+
+	log.Info("Hello Walrus before DisableTimestamp=true")
+	log.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
+	log.Info("Hello Walrus after FullTimestamp=true")
+
 	log.WithFields(log.Fields{
 		"animal": "walrus",
 		"size":   10,
 	}).Info("A group of walrus emerges from the ocean")
+
+	logger.WithFields(log.Fields{
+		"whichlogger": "logger",
+		"size":        10,
+	}).Info("using logger")
 
 	log.WithFields(log.Fields{
 		"omg":    true,
@@ -37,7 +72,7 @@ func main() {
 	// the logrus.Entry returned from WithFields()
 	contextLogger := log.WithFields(log.Fields{
 		"common": "this is a common field",
-		"other": "I also should be logged always",
+		"other":  "I also should be logged always",
 	})
 
 	contextLogger.Warn("I'll be logged with common and other field")
