f00519c336d3d885aec60a4595fa4c8eac29320b
Randy Fay <randy@randyfay.com>
2017-10-05 17:30:50 -0600

    Try more complex stuff with json

--- cmd/logrustry/main.go
+++ cmd/logrustry/main.go
@@ -8,7 +8,7 @@ import (
 // Overall strategy:
 /**
 * Add formatters for user interaction, normal logging, and full json interaction
-* Use log for all output aimed at the user
+* Use logrus (UserOut) for all output aimed at the user
 * Remove all imports of the stdlib log
 * Remove all usages of fmt.Print* and the like
 
@@ -39,4 +39,26 @@ func main() {
 	log.Debug("log.Debug() - should not show")
 	log.Info("log.Info() - should show with timestamp, and with extra data if no terminal")
 
+	jsonF := new(log.JSONFormatter)
+	UserOut.Formatter = jsonF
+	fields := make(log.Fields)
+	fields["tag1"] = "data1"
+	type AppRow struct {
+		X      int;
+		Y      string;
+		Name   string;
+		Status string;
+		Timeup string;
+	}
+	type vertex struct {
+		X, Y, Z int;
+		A string
+	}
+	randommap := make(map[string]string)
+	randommap["r1key"] = "r1data"
+	randommap["r2key"] = "r2data"
+	fields["complexrow1"] = AppRow{0, "1", "myapp", "running", "sometime"}
+	fields["row2"] = vertex{1, 2, 3, "4"}
+	fields["randommap"] = randommap
+	UserOut.WithFields(fields).Print("And this is the message")
 }
